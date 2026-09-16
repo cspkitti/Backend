eace5498ed8c36f4547b94336edec6f967342bb1
cspkitti <csapokitti-50035@taszi.hu>
2026-09-16 13:28:15 +0200

    ár és cím szerint
    paraméterezett végpontok + össz

--- konyvek/index.js
+++ konyvek/index.js
@@ -6,7 +6,10 @@ const PORT = 3030;
 //importálás!!!
 const {
     getKonyvek,
-    getKonyvById
+    getKonyvById,
+    getKonyvekByAr,
+    getKonyvDarab,
+    getKonyvByCim
 } = require("./services/konyvekservices")
 
 // ez kell ahhoz, hogy request body-t tudjak küldeni
@@ -17,10 +20,23 @@ app.get("/", (req, res) => {
     res.status(200).json({info: "Könyvek backend alkalmazás"})
 });
 
+// a paraméterezett végpontokat mindig alulra kell írni!
 //routes
 app.get("/konyvek", getKonyvek);
+
+app.get("/konyvek/db", getKonyvDarab);
+
 // fontos a paramsnál is az legyen ami a kettőspont után
+// amikor ellenőrzöd akkor csak a számot kell beírni az id már nem kell oda
+// paraméter = /:id
+// egy végpontnak több paramétere is lehet
 app.get("/konyvek/:id",getKonyvById);
+
+app.get("/konyvek/ar/:ar",getKonyvekByAr);
+
+app.get("/konyvek/cim/:cim", getKonyvByCim)
+
+
 
 // ez indítja az appot
 app.listen(PORT, () => {
--- konyvek/services/konyvekservices.js
+++ konyvek/services/konyvekservices.js
@@ -5,13 +5,53 @@ const getKonyvek = (req, res) =>{
 }
 
 // add vissza az 5-ös id könyvet
+// paraméterezés
+// paraméter = olyan mint egy változó csak a függvényeknek
 const getKonyvById = (req, res) => {
+    // ez egy objektum = req.params.id(alapértelmezetten string)
     let paramId = Number(req.params.id);
     let konyv = konyvek.filter(konyv => konyv.id == paramId);
+    //végpontoknak a válasza a kérdésre
     res.json(konyv);
 }
 
+const getKonyvekByAr = (req, res) => {
+    let paramAr = Number(req.params.ar);
+    let konyvekByAr = konyvek.filter(konyv => konyv.price < paramAr);
+    res.json(konyvekByAr);
+}
+
+//1.feladat: Készíts egy GET /konyvek/db végpontot, amely visszaadja, hány könyv található a tömbben.
+const getKonyvDarab = (req, res) => {
+    res.json({darab: konyvek.length})
+}
+
+// 2.feladat: Készíts egy GET /konyvek/cim/:cim végpontot, amely megkeresi a megadott című könyvet.
+const getKonyvByCim = (req, res) => {
+    // paramétert kisbetűsre, szóköz nélkülire
+    // könyv címet is kisbetűsre, szóköz nélkülire
+
+    const cim = req.params.cim.toLowerCase().trim();
+    //console.log(cim);
+    const konyv = konyvek.filter(konyv => {
+        let kisbetus = konyv.title.toLowerCase().replaceAll(' ', '');
+        if (kisbetus == cim){
+            return konyv
+        }
+    });
+    /*  SQLben
+        SELECT *
+        FROM konyvek
+        WHERE LOWER(title) = ${parameter}
+    */
+    res.json(konyv);
+}
+
+// exportálás!!!
 module.exports = {
     getKonyvek,
-    getKonyvById
+    getKonyvById,
+    getKonyvekByAr,
+    getKonyvDarab,
+    getKonyvByCim
 }
